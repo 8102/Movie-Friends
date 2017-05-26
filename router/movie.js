21ad622318048aa55254c7900d54fd40e74c221b
Hugo Prenat <hugo.prenat@epitech.eu>
2017-05-26 17:58:09 +0200

    Add RDFa attribute to html

--- router/movie.js
+++ router/movie.js
@@ -6,31 +6,32 @@ module.exports = function movie (req, res) {
   if (!req.session.authenticated) {
     res.redirect('/login');
   }
+  else {
+    var sum = [0, 0, 0, 0, 0];
 
-  var sum = [0, 0, 0, 0, 0];
-
-  database.getMovie(req.params.id, function (movie) {
-    if (movie.id != -1) {
-      database.getRatingsFromId(movie.id, function (ratings) {
-        for (var i = 0; i < ratings.length; i++) {
-          sum[ratings[i].rating - 1]++;
-        }
-        var rating = {
-          one: sum[0],
-          two: sum[1],
-          three: sum[2],
-          four: sum[3],
-          five: sum[4]
-        };
-        res.render('movie.hbs', {
-          movie: movie,
-          rating: rating,
-          session: req.session
+    database.getMovie(req.params.id, function (movie) {
+      if (movie) {
+        database.getRatingsFromId(movie.id, function (ratings) {
+          for (var i = 0; i < ratings.length; i++) {
+            sum[ratings[i].rating - 1]++;
+          }
+          var rating = {
+            one: sum[0],
+            two: sum[1],
+            three: sum[2],
+            four: sum[3],
+            five: sum[4]
+          };
+          res.render('movie.hbs', {
+            movie: movie,
+            rating: rating,
+            session: req.session
+          });
         });
-      });
-    }
-    else {
-      res.redirect(404, '/movies');
-    }
-  });
+      }
+      else {
+        res.redirect(404, '/movies');
+      }
+    });
+  }
 };
